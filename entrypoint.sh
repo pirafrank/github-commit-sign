1d16284404ccd3fe0d7d9737ae10898fb1f3e248
Francesco Pira <dev@fpira.com>
2024-04-30 15:50:26 +0200

    added warning in Docker env for missing gh token

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
 set -e
+
+if [ -z "$GITHUB_TOKEN" ]; then
+  echo "GITHUB_TOKEN is not set. Exiting."
+  exit 1
+fi
+
 argv=(node /app/github.js "$@")
 cmd=$(printf '%q ' "${argv[@]}")
 eval $cmd
